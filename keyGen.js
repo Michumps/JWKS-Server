ebe9afcbdeb2ebcc41fe6a20376a35029444975b
Michael Meyers <michaelmeyers409111@gmail.com>
2026-02-12 14:32:21 -0600

    added key and metadata storage

--- keyGen.js
+++ keyGen.js
@@ -16,19 +16,41 @@ async function initStorage() {
 }
 
 // Creates an RSA priv/pub key pair, to be used to sign JWTs
-crypto.generateKeyPair('rsa', {
-	modulusLength: 2048,
-	publicKeyEncoding: {
-		type: 'spki',
-		format: 'pem'
-	},
-	privateKeyEncoding: {
-		type: 'pkcs1',
-		format: 'pem'
-	},
-}, (err, publicKey, privateKey) => {
-	console.log(`TESTING ONLY: ${publicKey}`);
-});
+async function genAndStoreKeys() {
+	const {publicKey, privateKey} = crypto.generateKeyPairSync('rsa', {
+		modulusLength: 2048,
+		publicKeyEncoding: {
+			type: 'spki',
+			format: 'pem'
+		},
+		privateKeyEncoding: {
+			type: 'pkcs1',
+			format: 'pem'
+		},
+	}, (err, publicKey, privateKey) => {});
 
+	const kid = Date.now(); // timestamp for kid
+	const exp = Math.floor((Date.now() / 1000) + 3600); // expiry set to one hour (for now)
 
+	console.log(`kid ${kid} and exp ${exp} successfully created`);
 
+	const metadata = {
+		kid,
+		exp,
+		created: Math.floor(Date.now() / 1000)
+	};
+
+	// creates .pem file for private key using restricted file permissions
+	const keyPath = path.join(KEY_DIR, `key_${kid}.pem`);
+	await fs.writeFile(keyPath, privateKey, {mode: 0o600});
+	
+	// creates .json file for priv key metadata
+	const metadataPath = path.join(KEY_DIR, `key_${kid}.json`);
+	await fs.writeFile(metadataPath, JSON.stringify(metadata, null, 2), {mode: 0o600});
+
+	console.log(`Successfully generated key-pair with kid ${kid}`);
+
+	return {kid, privateKey, publicKey, exp};
+};
+
+genAndStoreKeys();
