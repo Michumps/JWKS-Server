267ed3a2a904d45ad6b05a1514c185873385169d
Michael Meyers <michaelmeyers409111@gmail.com>
2026-03-23 12:19:22 -0500

    Implemented database integration

--- JWTAuth.js
+++ JWTAuth.js
@@ -1,26 +1,34 @@
 const jwt = require('jsonwebtoken');
 const {loadKeys, loadExpKeys, genAndStoreKeys} = require('./keyGen');
 
-async function signJWT(payload, expired = false) {
+function signJWT(payload, expired = false) {
 	let keys;
 	let JWTexpiry;
 
+	// Could clean up logic and use ternary operator with functions for exp or valid; IMPROVE
 	if (expired) {
 		// check for expired keys, if there is none generate one
-		await loadExpKeys().then(async expKeys => {
-			if (expKeys.length === 0) await genAndStoreKeys(-1);
-		});
+		let expKeys = loadExpKeys();
 
-		const expKeys = await loadExpKeys(); // should always have exp key
+		if(expKeys.length === 0) {
+			const expKey = genAndStoreKeys(-1);
+			expKeys = [expKey];
+		}
+
+		// final check to see if there are expired keys (always should be)
+		if (expKeys.length === 0) throw new Error('No expired keys available for signing');
 
 		keys = expKeys;
 		JWTexpiry = '-1h'; // issue already expired JWT when using expired key
 	} else {
-		await loadKeys().then(async validKeys=> {
-			if (validKeys.length === 0) genAndStoreKeys();
-		}); // load valid keys
 
-		let validKeys = await loadKeys();
+		// check for valid keys, if there are none generate one
+		let validKeys = loadKeys();
+
+		if (validKeys.length === 0) {
+			const validKey = genAndStoreKeys();
+			validKeys = [validKey];
+		}
 
 		// check if there are valid keys (always should be)
 		if (validKeys.length === 0) throw new Error('No valid keys available for signing');
