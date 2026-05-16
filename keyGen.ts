d99102fd665059bed2ccb77b5db210dd4a7fbdc6
Michael Meyers <michaelmeyers409111@gmail.com>
2026-05-16 16:36:30 -0500

    Restructured file to TypeScript

---
+++ keyGen.ts
@@ -0,0 +1,55 @@
+import Database from 'better-sqlite3';
+import crypto from 'crypto';
+import type { KeyRow } from './database.js';
+
+/*
+	Should work on cleaning up the way that Key expiry is handled.
+	Shouldn't be hardcoded in a module level variable that is not
+	able to be controlled from the server. TO_DO
+*/ 
+
+const KEY_EXP = 2; // explicit key expiry, defined in hours
+
+// Creates an RSA priv/pub key pair, to be used to sign JWTs
+function genAndStoreKeys(genExpired = 1, database: Database.Database) {
+	const {publicKey, privateKey} = crypto.generateKeyPairSync('rsa', {
+		modulusLength: 2048,
+		privateKeyEncoding: {
+			type: 'pkcs1',
+			format: 'pem'
+		},
+	});
+
+	const kid = Date.now(); // timestamp for kid
+	const exp = Math.floor((Date.now() / 1000) + ((KEY_EXP * 3600) * genExpired)); // expiry set based on KEY_EXP and genExpired
+
+	// Inserts private key into database
+	database.prepare('INSERT INTO keys (kid, key, exp) VALUES (?, ?, ?)').run(kid, privateKey, exp);
+
+	console.log(`Successfully generated key-pair with kid ${kid}`);
+};
+
+function loadKeys(database: Database.Database, expired: 'valid' | 'expired' = 'valid'):KeyRow[] {
+	const currentTime = Math.floor((Date.now() / 1000));
+
+	let keys:KeyRow[];
+
+	if (expired === 'valid') {
+		keys = database.prepare<[number], KeyRow>('SELECT * FROM keys WHERE exp > ?').all(currentTime);
+	} else {
+		keys = database.prepare<[number], KeyRow>('SELECT * FROM keys WHERE exp < ?').all(currentTime);
+	}
+
+	return keys;
+}
+
+function cleanExpiredKeys(database: Database.Database) {
+	const currentTime = Math.floor(Date.now() / 1000); // convert to seconds
+
+	const info = database.prepare('DELETE FROM keys WHERE exp < ?').run(currentTime);
+
+	console.log(`Successfully deleted ${info.changes} expired keys`);
+}
+
+// exports functions to be used in other files
+export {genAndStoreKeys, loadKeys, cleanExpiredKeys,}
