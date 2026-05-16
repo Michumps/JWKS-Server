1c6a76741eec57eaba89a32414ab2cf116da1c3d
Michael Meyers <michaelmeyers409111@gmail.com>
2026-05-16 16:31:58 -0500

    Restructured file to use TypeScript

---
+++ database.ts
@@ -0,0 +1,33 @@
+import fs from 'fs'; // used for directory creating and file permissions
+import path from 'path'
+import Database from 'better-sqlite3';
+
+
+function initDatabase(dbFile: string | undefined):Database.Database {
+	
+	// dbFile can be used instead; for testing
+	const finalFile = dbFile || 'totally_not_my_privateKeys.db'
+	const finalPath = path.join(__dirname, 'data', finalFile);
+
+	// Ensures that data/ is created if doesn't exist
+	const dir = path.dirname(finalPath);
+	if (!fs.existsSync(dir)) {
+		fs.mkdirSync(dir, { recursive: true });
+	}
+
+	let db = new Database(finalPath);
+
+	db.exec(`
+		CREATE TABLE IF NOT EXISTS keys(
+			kid INTEGER PRIMARY KEY,
+			key TEXT NOT NULL,
+			exp INTEGER NOT NULL
+		)
+		`);
+
+	console.log("Database successfully created")
+
+	return db;
+}
+
+export default initDatabase;
